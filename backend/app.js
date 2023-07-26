756b5625c698347bf2a7363076009227af2d27de
Saikiran Patil <patilrsaikiran@gmail.com>
2023-07-26 11:58:04 +0530

    added database.js and vercel.json file

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const Document = require("./Document");
+const connectDB = require("./database");
 
 dotenv.config({ path: "./.env" });
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+
+// Connect to Database
+connectDB();
 
 const io = require("socket.io")(3001, {
     cors: {
